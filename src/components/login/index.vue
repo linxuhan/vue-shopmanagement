c1f642a2a40b54108c376e28514db22039e57a6e
linxuhan <768445037@qq.com>
2020-10-29 23:49:45 +0800

    登录页面

---
+++ src/components/login/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="login-container">
+      <div class="login-box">
+        <div class="login_avatar">
+          <img src="../../assets/user.jpg" alt="">
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  height: 100%;
+  background-color: #222222;
+}
+
+.login-box {
+  width: 450px;
+  height: 350px;
+  background-color: #fff;
+  border-radius: 2px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .login_avatar {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+    }
+  }
+}
+</style>
